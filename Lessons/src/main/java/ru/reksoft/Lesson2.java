55626e846b9d9ff763daff6f92100c8b99b59ab4
Tatyana1985 <tatuana.chunikhina@yandex.ru>
2018-05-10 12:00:23 +0300

    домашка 2

--- Lessons/src/main/java/ru/reksoft/Lesson2.java
+++ Lessons/src/main/java/ru/reksoft/Lesson2.java
@@ -2,7 +2,15 @@ package ru.reksoft;
 
 class Lesson2 {
 
-    /**
+    public static void main(String[] args)
+    {
+
+        System.out.println(formula(4));
+        System.out.println(initials("иванов Петр Александрович"));
+
+    }
+
+      /**
      * Нужно реализовать формулу y = √(13x + 13/x)
      * Подсказка: квадратный корень вычисляется функцией Math.sqrt()
      *
@@ -10,9 +18,14 @@ class Lesson2 {
      * @return возвращаем только целую часть от полученного результата,
      * хвост отбрасываем без откругления
      */
-    static int formula(int x) {
-        return 0;
+    static int formula(int x)
+    {
+        double y;
+        y = Math.sqrt(13*x + 13/x);
+        int f = (int) y;
+        return f;
     }
+
 
     /**
      * Нужно привести строку с полным именем к инициалам.
@@ -21,13 +34,16 @@ class Lesson2 {
      * разделение строки на части: .split(" ");
      * получение символа на определенной позиции: .charAt();
      * приведение к верхнему регистру: .toUpperCase();
-     *
-     * @param fullName на текущем этапе имена будут состоять из имени, фамили и отчества, разделяться пробелами.
-     *                 Регистр букв во входном параметре может быть любым.
+     *toUpperCase()дном параметре может быть любым.
      * @return возвращаем инициалы в верхнем регистре разделенные точкой, как в примере выше
      */
-    static String initials(String fullName) {
-        return null;
+    static String initials(String fullName)
+    {
+        String initial = fullName.split(" ")[0].charAt(0) + "." +
+                fullName.split(" ")[1].charAt(0)+ "."
+                + fullName.split(" ")[2].charAt(0) + ".";
+        initial = initial.toUpperCase();
+        return initial;
     }
 
 }
